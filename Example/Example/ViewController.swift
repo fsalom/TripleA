dcbe6aa1845dfc329f06d75c81b757e5e1078368
Fernando Salom <Fdosalom@gmail.com>
2022-08-15 20:16:12 +0200

    Add example project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Fernando Salom Carratala on 15/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
