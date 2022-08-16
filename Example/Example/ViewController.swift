640624b4cbd50044b7c40b0a3ab8694fc6a772d4
Fernando Salom <Fdosalom@gmail.com>
2022-08-16 12:35:01 +0200

    Fix import Triple A for example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let x = AuthManager()
+        let x = AuthManager(baseURL: "", clientId: "", clientSecret: "")
         let y = Network(authManager: x)
         // Do any additional setup after loading the view.        
     }
