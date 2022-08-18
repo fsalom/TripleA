50f0e3b4fb08170ddb45fb4c619b0e18977ff857
Fernando Salom <Fdosalom@gmail.com>
2022-08-18 11:40:51 +0200

    Add new auth error

--- Sources/TripleA/Error/AuthError.swift
+++ Sources/TripleA/Error/AuthError.swift
@@ -3,4 +3,14 @@ public enum AuthError: Error {
     case missingExpiresIn
     case badRequest
     case tokenNotFound
+    case errorData(Data)
+
+    var data: Data {
+        switch self {
+        case .errorData(let data):
+            return data
+        default:
+            return Data()
+        }
+    }
 }
