a0c8b19aa0ffa3a2ddab5646ef567bbf95fd523b
Fernando Salom <fer@rudo.es>
2024-01-17 10:07:33 +0100

    Change package settings and move folders

--- Package.swift
+++ Package.swift
@@ -21,9 +21,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TripleA",
-            dependencies: []),
+            path: "Sources/"),
         .testTarget(
             name: "TripleATests",
-            dependencies: ["TripleA"]),
+            path: "Tests/"),
     ]
 )
