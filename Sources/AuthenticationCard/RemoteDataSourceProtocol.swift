339feb8a4399ad91f8b0e0d07016268d92c6bc45
Fernando Salom <fer@rudo.es>
2024-04-21 18:54:04 +0200

    Add throw for logout

--- Sources/AuthenticationCard/RemoteDataSourceProtocol.swift
+++ Sources/AuthenticationCard/RemoteDataSourceProtocol.swift
@@ -1,5 +1,5 @@
 public protocol RemoteDataSourceProtocol {
     func getAccessToken(with parameters: [String: Any]) async throws -> String
     func getRefreshToken(with refreshToken: String) async throws -> String
-    func logout() async
+    func logout() async throws
 }
