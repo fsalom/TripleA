f38950f8172b24ec1dec082fe710ede675759c93
Fernando Salom <fer@rudo.es>
2024-04-27 19:51:52 +0200

    Add new swiftui example

---
+++ Example/SwiftUI/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Fernando Salom Carratala on 26/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
