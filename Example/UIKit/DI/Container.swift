5d89122963447ace953aea8734093ab852646797
Fernando Salom <fer@rudo.es>
2024-05-01 11:39:24 +0200

    Fix example with new flow

--- Example/UIKit/DI/Container.swift
+++ Example/UIKit/DI/Container.swift
@@ -14,10 +14,10 @@ class Container {
                                     "client_id": "1gzyJeSOyjUOmbSHREbsothngkBMato1VypQz35D",
                                     "client_secret": "ynM8CpvlDHivO1jma1Q3Jv1RIJraBbJ9EtK5XI3dw4RpkxDgi9cZnmJlQs0XzuVCGWCNwQd8qJKAHFrLdHlRRDIzx8B08HJ0Htu6XFzP4kTRTWYIPHuCpldjouJhKvoA"]
     //REMOTE DATA SOURCE
-    static let remoteDataSource = OAuthGrantTypePasswordManager(storage: Container.storage,
-                                                                startController: getLoginController(),
-                                                                refreshTokenEndpoint: OAuthAPI.refresh(parametersRefresh).endpoint,
-                                                                tokensEndpoint: OAuthAPI.login(parametersLogin).endpoint)
+    static let oauthCard = OAuthGrantTypePasswordManager(storage: Container.storage,
+                                                         startController: getLoginController(),
+                                                         refreshTokenEndpoint: OAuthAPI.refresh(parametersRefresh).endpoint,
+                                                         tokensEndpoint: OAuthAPI.login(parametersLogin).endpoint)
 
     static let config = PKCEConfig(clientID: "vW6QyPMdId5NlGRNqbULm0YFWk0hq6VMWBDWSovp",
                                    clientSecret: "9ElafIEJbecCbk83t0SNYobrSMdD6mKUvgIwOE1vkWOqHvqhcWtCBZV1tGB9wJNi81EYLUwV8kSfIbQb12AhZAgGCl8gfpYh5Tr8o45iuNC97Ucizac32qySLe9lhdJ3",
@@ -29,16 +29,16 @@ class Container {
                                    responseType: "code",
                                    callbackURLScheme: "app://auth",
                                    callbackURLLogoutScheme: "app://auth")
-    static let PKCEDataSource = PKCEManager(storage: Container.storage, presentationAnchor: nil, config: config )
+    static let PKCECard = PKCEManager(storage: Container.storage, presentationAnchor: nil, config: config )
     //AUTHMANAGER
     let authManager = AuthManager(storage: Container.storage,
-                                  card: Container.PKCEDataSource,
+                                  card: Container.oauthCard,
                                   parameters: [:])
     //NETWORK
     static let network = Network(baseURL: "https://dashboard-staging.rudo.es/",
                                  authManager: Container.shared.authManager,
                                  format: .full)
-    
+
     init() { }
 }
 
