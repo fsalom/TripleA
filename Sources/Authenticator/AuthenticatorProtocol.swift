4942ffa942bd8a35de34b0a4f4a128b826f16336
Fernando Salom <fer@rudo.es>
2024-05-14 22:24:25 +0200

    Change authenticator protocol to adapt actor model

--- Sources/Authenticator/AuthenticatorProtocol.swift
+++ Sources/Authenticator/AuthenticatorProtocol.swift
@@ -1,7 +1,13 @@
+public enum TokenType {
+    case access
+    case refresh
+}
+
 public protocol AuthenticatorProtocol {
-    var storage: TokenStorageProtocol { get set }
     func getCurrentToken() async throws -> String
     func getNewToken(with parameters: [String: Any]) async throws
     func renewToken() async throws -> String
     func logout() async throws
+    func get(token type: TokenType) async throws -> Token?
+    func set(token: Token, for type: TokenType) async throws
 }
