c328f0f9598d9d87f8a0cfc69c3061fc98fd96ae
Fernando Salom <fer@rudo.es>
2024-05-17 12:44:10 +0300

    Remove authenticator wrapper

--- Sources/AuthenticatorWrapper/AuthenticatorWrapperProtocol.swift
+++ Sources/AuthenticatorWrapper/AuthenticatorWrapperProtocol.swift
@@ -1,10 +1,1 @@
-import Foundation
 
-public protocol AuthenticatorWrapperProtocol {
-    func isLogged() async -> Bool
-    func getCurrentToken() async throws -> String
-    func getNewToken(with parameters: [String: Any]) async throws
-    func logout() async throws
-    func get(token type: TokenType) async throws -> Token?
-    func set(token: Token, for type: TokenType) async throws
-}
