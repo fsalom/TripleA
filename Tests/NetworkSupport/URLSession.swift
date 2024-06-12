63576ab32331d12116b666b909ac15b74aac2366
Fernando Salom <fer@rudo.es>
2024-06-12 09:20:45 +0200

    Add extension URLSession for tests

---
+++ Tests/NetworkSupport/URLSession.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+internal extension URLSession {
+    convenience init<T: MockURLResponder>(mockResponder: T.Type) {
+        let config = URLSessionConfiguration.ephemeral
+        config.protocolClasses = [MockURLProtocol<T>.self]
+        self.init(configuration: config)
+        URLProtocol.registerClass(MockURLProtocol<T>.self)
+    }
+}
