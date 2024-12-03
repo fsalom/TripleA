d9d54fd7ca835472b074737b14402d8f258292e3
Fernando Salom <fer@rudo.es>
2024-12-03 09:33:48 +0100

    Add new state loading

--- Sources/AuthenticatorWrapper/SwiftUI/Screen.swift
+++ Sources/AuthenticatorWrapper/SwiftUI/Screen.swift
@@ -3,11 +3,13 @@ import Foundation
 public enum Screen {
     case login
     case home
+    case loading
 
     var icon: String {
         switch self {
         case .login: "🛂"
         case .home: "🔒"
+        case .loading: "⏳"
         }
     }
 }
